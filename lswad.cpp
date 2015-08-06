c9889f275aed7a97dae1440e3004fcf553e64d9e
brycekelleher <bryce_kelleher@hotmail.com>
2015-08-06 12:18:05 +1000

    Update lswad.cpp
    remove unused error function

--- lswad.cpp
+++ lswad.cpp
@@ -3,24 +3,6 @@
 #include <stdarg.h>
 #include "doomlib.h"
 
-static void Error(const char *format, ...)
-{
-	#define BUFFER_SIZE	1024
-
-        va_list valist;
-        char buffer[BUFFER_SIZE];
-
-        va_start(valist, format);
-        vsnprintf(buffer, BUFFER_SIZE, format, valist);
-        va_end(valist);
-
-        fprintf(stderr, "\x1b[31m");
-        fprintf(stderr, "Error: %s", buffer);
-        fprintf(stderr, "\x1b[0m");
-	fflush(stderr);
-        exit(1);
-}
-
 void IterateCallback(int lumpnum, char name[8], int size)
 {
 	printf("%4i %-12.8s %i / %#x\n", lumpnum, name, size, size);
